bfaf8f989e31db1d984b6dfa6c2dd1ba00e4d318
Mohit ASHAR <mohitashar1995@gmail.com>
2020-09-25 00:19:41 +0200

    Added sqrt function

--- solve.c
+++ solve.c
@@ -6,18 +6,34 @@
 /*   By: mashar <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/24 19:26:17 by mashar            #+#    #+#             */
-/*   Updated: 2020/09/24 20:59:29 by mashar           ###   ########.fr       */
+/*   Updated: 2020/09/24 23:56:23 by mashar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+
+double		ft_sqrt(double number)
+{
+	double sqrt;
+	double temp;
+
+	sqrt = number / 2;
+	temp = 0;
+	while (sqrt != temp)
+	{
+		temp = sqrt;
+		sqrt = (number / temp + temp) / 2;
+	}
+	return (sqrt);
+}
 
 void		solve_two_degree(double *numbers, double **solution)
 {
-	double discriminant = (numbers[1] * numbers[1]) - 4 * numbers[0] * \
-						  numbers[2];
+	double discriminant;
+
+	discriminant = (numbers[1] * numbers[1]) - 4 * numbers[0] * \
+						numbers[2];
 	printf("Disc is %lf\n", discriminant);
 	if (discriminant == 0)
 	{
@@ -30,12 +46,14 @@ void		solve_two_degree(double *numbers, double **solution)
 		discriminant = discriminant * -1;
 		solution[0][0] = -1;
 		solution[0][1] = (-1 * numbers[1]) / (2 * numbers[2]);
-		solution[0][2] = sqrt(discriminant) / (2 * numbers[2]);
+		solution[0][2] = ft_sqrt(discriminant) / (2 * numbers[2]);
 		return ;
 	}
 	solution[0][0] = 2;
-	solution[0][1] = (-1 * numbers[1] + sqrt(discriminant)) / (2 * numbers[2]);
-	solution[0][2] = (-1 * numbers[1] - sqrt(discriminant)) / (2 * numbers[2]);
+	solution[0][1] = (-1 * numbers[1] + \
+			ft_sqrt(discriminant)) / (2 * numbers[2]);
+	solution[0][2] = (-1 * numbers[1] - \
+			ft_sqrt(discriminant)) / (2 * numbers[2]);
 }
 
 double		*solve(double *numbers, int degree)
@@ -49,7 +67,6 @@ double		*solve(double *numbers, int degree)
 			return (NULL);
 		solution[0] = 1;
 		solution[1] = numbers[0] / numbers[1];
-		//solve_one_degree(numbers, &solution);
 		return (solution);
 	}
 	else
@@ -62,14 +79,14 @@ double		*solve(double *numbers, int degree)
 	}
 }
 
-int main()
+int			main()
 {
-	double numbers[5] = {4, 4, -9.3, 0, 0};
+	double numbers[5] = {3, 22, 20, 0, 0};
 	double *solution;
 	int degree = 2;
 	solution = solve(numbers, degree);
 	if(degree == 1)
-		printf("Solution is %lf\n", solution[0]);
+		printf("Solution is %lf\n", solution[1]);
 	else
 	{
 		if (solution[0] == 1)
